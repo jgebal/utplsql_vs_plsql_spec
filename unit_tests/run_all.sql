adc95d16077fd7f18a5d4dd57ab3297cdc7b9d09
Jacek Gębal <jgebal@gmail.com>
2015-06-24 00:24:15 +0100

    Reorganized test and folder structure

--- unit_tests/run_all.sql
+++ unit_tests/run_all.sql
@@ -1,7 +1,14 @@
 BEGIN
-  utplsql.test('sum_two_numbers', recompile_in => FALSE);
-  utplsql.test('sum_two_numbers_fail', recompile_in => FALSE);
-  utplsql.test('current_date', recompile_in => FALSE);
-  utplsql.test('current_date_fail', recompile_in => FALSE);
+  utplsql.test('sum_two_numbers');
+  utplsql.test('sum_two_numbers_fail');
+  utplsql.test('current_date');
+  utplsql.test('current_date_fail');
+END;
+/
+
+BEGIN
+  utConfig.autocompile(false);
+  utConfig.setFileDir('/tmp');
+  utplsql.test('message_api');
 END;
 /
