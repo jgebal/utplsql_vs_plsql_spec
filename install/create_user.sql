c1d57c5f2a1fad470295025088a0a26de7f276f4
Jacek Gębal <jgebal@gmail.com>
2015-06-27 08:13:17 +0100

    Added missing privilege for users, so they can see all sources

--- install/create_user.sql
+++ install/create_user.sql
@@ -2,8 +2,8 @@ create user &1 identified by &1 default tablespace
   users temporary tablespace temp;
 
 grant create session, create table, create procedure,
-create sequence, create view, create public synonym,
-drop public synonym to &1;
+create sequence, create view, create public synonym, select any dictionary,
+create any procedure, drop public synonym to &1;
 
 alter user &1 quota unlimited on users;
 
