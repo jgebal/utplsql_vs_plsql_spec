e71fd44b798cb8781d9a4c993141eefe06101580
Jacek Gębal <jgebal@gmail.com>
2015-07-11 15:54:35 +0100

    Added configuration for Unit Test suite, so that all UTPLSQL tests can be executed with Jenkins.

--- unit_tests/run_all.sql
+++ unit_tests/run_all.sql
@@ -33,6 +33,17 @@ BEGIN
   utplsql.test('sum_two_numbers_fail');
   utplsql.test('current_date');
   utplsql.test('current_date_fail');
+  utplsql.test('message_api');
+END;
+/
+
+BEGIN
+  utsuite.add('all');
+  utpackage.add('all','sum_two_numbers');
+  utpackage.add('all','sum_two_numbers_fail');
+  utpackage.add('all','current_date');
+  utpackage.add('all','current_date_fail');
+  utpackage.add('all','message_api');
 END;
 /
 
