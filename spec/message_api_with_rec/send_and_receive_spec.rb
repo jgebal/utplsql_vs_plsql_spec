bf512af009293b4dc9033f11d93db14c418b28a0
Jacek Gębal <jgebal@gmail.com>
2015-11-09 23:29:43 +0000

    differentiated samples

--- spec/message_api_with_rec/send_and_receive_spec.rb
+++ spec/message_api_with_rec/send_and_receive_spec.rb
@@ -7,13 +7,13 @@ describe 'cross session communication package message_api_with_rec' do
   end
 
   it 'allows sending and receiving messages across different sessions of the same user' do
-    a_message = { msg_number: 1, msg_text: 'a message', msg_date: Time.today }
+    a_message = { msg_number: 2, msg_text: 'different message', msg_date: Time.today - 1 }
     plsql(:main_session).message_api_with_rec.send_msg( a_message )
     expect( plsql(:another_session).message_api_with_rec.receive_msg ).to eq( a_message )
   end
 
   it 'allows sending and receiving messages across sessions of different users' do
-    a_message = { msg_number: 1, msg_text: 'a message', msg_date: Time.today }
+    a_message = { msg_number: 3, msg_text: 'message for another user', msg_date: Time.today + 1 }
     plsql.message_api_with_rec.send_msg( a_message )
     expect( plsql(:different_user).message_api_with_rec.receive_msg ).to eq( a_message )
   end
